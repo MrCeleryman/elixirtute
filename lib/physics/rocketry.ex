1546f16c9da54e0776991b882d9e70f4bb744719
MrCeleryman <danielisbread@gmail.com>
2017-04-29 15:52:13 +1000

    splitting up code

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,30 +1,13 @@
-defmodule Converter do
-	
-	def to_nearest_tenth(val) do
-		Float.ceil(val, 1)
-	end
-
-	def to_km(velocity) do
-		velocity / 1000
-	end
-
-	def to_light_seconds({:miles, miles}) do
-		(miles * 5.36819e-6) |> round_down
-	end
-
-	def to_light_seconds({:meters, meters}) do
-		(meters * 3.335638620368e-9) |> round_down
-	end
-
-	def round_down(val) when is_float(val), do: trunc(val)
-end
-
 defmodule Physics.Rocketry do
 
-	def escape_velocity(:earth) do
-		%{mass: 5.972e24, radius: 6.371e6}
-			|> escape_velocity
-	end
+	import Converter
+	import Calcs
+	import Physics.Laws
+	import Planets
+
+	def escape_velocity(:earth), do: earth |> escape_velocity
+	def escape_velocity(:mars), do: mars |> escape_velocity
+	def escape_velocity(:moon), do: moon |> escape_velocity
 
 	def escape_velocity(planet) when is_map(planet) do
 		planet
@@ -34,9 +17,8 @@ defmodule Physics.Rocketry do
 	end
 
 	defp calculate_escape(%{mass: mass, radius: radius}) do
-		newtons_constant = 6.67e-11
-		2 * newtons_constant * mass / radius
-			|> :math.sqrt
+		2 * newtons_gravitational_constant * mass / radius
+			|> square_root
 	end
 
 end
